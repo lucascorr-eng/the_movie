be66b722b85a1a1a77df75152608bcb879d031e8
Lucas Mint <lucas.engsoftwareee@gmail.com>
2025-11-27 15:22:48 -0300

    new ideia for select the movies

--- script.js
+++ script.js
@@ -12,3 +12,23 @@ buttonClose.onclick = function () {
   modal.close()
 }
 
+
+
+const movie1 = document.querySelector("#item1")
+
+movie1.addEventListener("click", function() {
+  modal.showModal()
+})
+
+
+const listMovie = document.querySelector(".container")
+const movies = listMovie.querySelectorAll("div")
+
+movies.forEach( movieSelct => {
+  movieSelct.addEventListener("click", function(eventet) {
+    const movieCLick = event.target;
+    console.log("a div clickada foi o", movieCLick.id)
+
+    modal.showModal()
+  })
+})
